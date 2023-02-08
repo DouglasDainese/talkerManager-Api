14bf70bdec8e6931af7b5e62ac8bec1efbe79db8
DouglasDainese <douglas_dainese@hotmail.com>
2023-02-07 21:55:02 -0300

    feat: A requisição deve retornar o status 200 e um array com todas as pessoas palestrantes cadastradas e um array vazio caso não tenha ninguem cadastrado

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs/promises');
+const path = require('path');
 // iniciando o projeto
 
 const app = express();
@@ -10,6 +12,13 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const contentPath = path.resolve(__dirname, 'talker.json');
+  const data = await fs.readFile(contentPath, 'utf-8');
+  const talkersData = JSON.parse(data);
+ return res.status(HTTP_OK_STATUS).json(talkersData);
 });
 
 app.listen(PORT, () => {
